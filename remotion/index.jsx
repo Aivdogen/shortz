f4045a66c35013789a5b7f65d1d9c3ef0b911555
Aivdogen <aivideogen03@gmail.com>
2024-10-13 15:53:34 +0530

    Update index.jsx

--- remotion/index.jsx
+++ remotion/index.jsx
@@ -1,4 +1,5 @@
-import { registerRoot } from "remotion";
-import { RemotionRoot } from "./Root";
- 
+// remotion/index.jsx
+import { registerRoot } from 'remotion';
+import RemotionRoot from './Root'; // Ensure the path is correct
+
 registerRoot(RemotionRoot);
