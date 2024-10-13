cc668d42fe31abc100f6e0a46d021b6c661009c2
Aivdogen <aivideogen03@gmail.com>
2024-10-13 15:53:14 +0530

    Update Root.jsx

--- remotion/Root.jsx
+++ remotion/Root.jsx
@@ -1,20 +1,19 @@
-import RemotionVideo from '@/app/dashboard/_components/RemotionVideo'
-import React from 'react'
-import { Composition } from 'remotion'
+import RemotionVideo from '../app/dashboard/_components/RemotionVideo';
+import { Composition } from 'remotion';
 
 function RemotionRoot() {
   return (
     <>
-    <Composition
-      id="Empty"
-      component={RemotionVideo}
-      durationInFrames={60}
-      fps={30}
-      width={1280}
-      height={720}
-    />
-  </>
-  )
+      <Composition
+        id="Empty" // This is your composition ID
+        component={RemotionVideo}
+        durationInFrames={60}
+        fps={30}
+        width={1280}
+        height={720}
+      />
+    </>
+  );
 }
 
-export default RemotionRoot
+export default RemotionRoot;
